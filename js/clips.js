f399e09dddd9b85313c12a818ca615c66df1dd48
Matt Basile <matthewbasile2@gmail.com>
2021-07-01 10:47:00 -0400

    final fix ups

--- js/clips.js
+++ js/clips.js
@@ -3,50 +3,50 @@ function clips(){
     localClips:[
       {
         title:"Tie-Dye Frittata",
-        publisher:"Recipe Development",
+        publisher:" - Recipe Development",
         link:"https://harvestclub.localrootsnyc.com/blogs/recipes/tie-dye-frittata?_pos=6&_sid=1adf32683&_ss=r",
       },
       {
         title:"Autumn Vegetable and White Bean Soup",
-        publisher:"Recipe Development",
+        publisher:" - Recipe Development",
         link:"https://harvestclub.localrootsnyc.com/blogs/recipes/autumn-vegetable-and-white-bean-soup?_pos=3&_sid=1adf32683&_ss=r",
       },
       {
         title:"Crispy Eggplant Coins w/ Yogurt Tahini Dip",
-        publisher:"Recipe Development",
+        publisher:" - Recipe Development",
         link:"https://harvestclub.localrootsnyc.com/blogs/recipes/asian-eggplant-coins-w-yogurt-tahini-dip?_pos=2&_sid=1adf32683&_ss=r",
       },
       {
         title:"Farming in the Era of Climate Change: An Interview with Lauren Nagy of Ironbound Farm",
-        publisher:"Blog Post",
+        publisher:" - Blog Post",
         link:"https://harvestclub.localrootsnyc.com/blogs/news/climate-week-an-interview-with-lauren-nagy-of-ironbound-farm?_pos=9&_sid=1adf32683&_ss=r",
       },
       {
         title:"Soil Health Impacts Flavor and Nutrition",
-        publisher:"Blog Post",
+        publisher:" - Blog Post",
         link:"https://harvestclub.localrootsnyc.com/blogs/news/soil-health-impacts-flavor-and-nutrition?_pos=11&_sid=1adf32683&_ss=r",
       },
       {
         title:"The Overlooked History of Regenerative Agriculture and CSAs in America",
-        publisher:"Blog Post",
+        publisher:" - Blog Post",
         link:"https://harvestclub.localrootsnyc.com/blogs/news/the-overlooked-history-of-regenerative-agriculture-and-csas-in-america?_pos=12&_sid=1adf32683&_ss=r",
       },
       {
         title:"How to continue eating with friends when picnic season is over",
-        publisher:"Blog Post",
+        publisher:" - Blog Post",
         link:"https://harvestclub.localrootsnyc.com/blogs/news/how-to-continue-eating-with-friends-when-picnic-season-is-over?_pos=14&_sid=1adf32683&_ss=r",
       },
     ],
     selectedClips:[
       {
         title:"Meet the Women Making Tinned Fish Cool",
-        publisher:"Condé Nast Traveler",
+        publisher:" - Condé Nast Traveler",
         link:"https://www.cntraveler.com/story/meet-the-women-making-tinned-fish-cool",
         // image:"https://media.cntraveler.com/photos/60596d8a7b588da524cfef4a/16:9/w_2560%2Cc_limit/SalmonSisters-2021-2.jpg",
       },
       {
         title:"9 North Fork food trucks for a delicious socially distanced date",
-        publisher:"The Northforker",
+        publisher:" - The Northforker",
         link:"https://northforker.com/2020/08/9-north-fork-food-trucks-for-a-delicious-socially-distanced-date/",
         // image:"https://i0.wp.com/northforker.com/files/2020/08/RollinginDough.jpeg?resize=768%2C1024&ssl=1",
       },
@@ -54,17 +54,17 @@ function clips(){
     careClips:[
       {
         title:"@careofchan",
-        publisher:"Copywriting and content management",
+        publisher:" - Copywriting and content management",
         link:"https://www.instagram.com/careofchan/?hl=en",
       },
       {
         title:"Eating in Isolation Podcast",
-        publisher:"Research and production",
+        publisher:" - Research and production",
         link:"https://podcasts.apple.com/gb/podcast/eating-in-isolation/id1564279560",
       },
       {
         title:"The Correspondence Newsletter",
-        publisher:"Contributor and editor",
+        publisher:" - Contributor and editor",
         link:"https://us16.campaign-archive.com/home/?u=5fb6bbf69133e43c81d868b55&id=36c5b4ed65",
       },
     ]
